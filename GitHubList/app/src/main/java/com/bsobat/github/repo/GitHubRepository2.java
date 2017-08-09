d5a3f3a023c51f2ccfa443cd696e24fe4a97d150
bsobat <bsobat@bol.com>
2017-08-09 15:27:53 +0200

    Removed unused method

--- GitHubList/app/src/main/java/com/bsobat/github/repo/GitHubRepository2.java
+++ GitHubList/app/src/main/java/com/bsobat/github/repo/GitHubRepository2.java
@@ -72,42 +72,6 @@ public class GitHubRepository2 {
         return liveData;
     }
 
-    @WorkerThread
-    private void refresh(final int page, final int limit) {
-
-        /**
-         * We will create an asynctask to retrieve the data from the server, online if needed.
-         * (this depends on usecase, in our case, if there is data, we wouldn't retrieve.
-         * If we need to refresh our db, we retrieve the data from the server and update
-         * our local database, Room will automatically notify the active observers (see above).
-         */
-        try {
-            int offset = (page - 1) * limit;
-            List<GitHubDto> list = dao.hasData(offset, limit);
-            if (list != null && !list.isEmpty()) {
-                //The data is cached, we don't need to go retrieve the data from the server.
-                Log.d("DATA", "From cache");
-                return;
-            }
-            Log.d("DATA", "Fetching from server: " + page + " , " + limit);
-            Response<List<GitHubDto>> response = api.browseRepo(page, limit).execute();
-            List<GitHubDto> body = response.body();
-            if (body != null) {
-                GitHubDto[] arr = new GitHubDto[body.size()];
-                body.toArray(arr);
-                //The data will be inserted into our database.
-                long[] ids = dao.insertAll(arr);
-                if (ids == null || ids.length != arr.length) {
-                    Log.e("API", "Unable to insert");
-                } else {
-                    Log.d("DATA", "Data inserted");
-                }
-            }
-        } catch (IOException e) {
-            Log.e("API", "" + e.getMessage());
-        }
-    }
-
     public void clearCache() {
         executor.execute(new Runnable() {
             @Override
